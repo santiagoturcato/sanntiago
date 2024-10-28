38484bc3e3407daff7b84421ae9c278d05da345d
santiagoturcato <163873307+santiagoturcato@users.noreply.github.com>
2024-10-28 11:37:24 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Educação Física',
+    'Quantos jogadores cada time pode ter em quadra em um jogo de futsal?',
+    "Cada time pode ter no máximo 5 jogadores, 1 goleiro e 4 na linha"
+)
+criaCartao(
+    "Biologia",
+    "Cite o nome das classes do reino animal",
+    "peixes, anfíbios, répteis, aves e mamíferos."
+)
+criaCartao(
+    'Geografia',
+    "Qual a capital do Chile?",
+    "Santiago"
+)
+criaCartao(
+    "Sociologia",
+    "De quem é a frase 'só sei que nada sei'",
+    "Sócrates"
+)
